70a8f6d4ba8cdf903855b5f9b77110f5ed0858ac
sgwood <sgwood@qq.com>
2023-06-12 18:24:04 +0800

    commit

--- src/views/ai/403.vue
+++ src/views/ai/403.vue
@@ -1,6 +1,12 @@
 <template>
   <Error type="403" @error-click="push('/')" />
+  <div class="xxx rounded-3xl"></div>
 </template>
 <script lang="ts" name="Error403" setup>
 const { push } = useRouter()
 </script>
+<style>
+.xxx {
+  @apply rounded-4xl;
+}
+</style>
---
+++ src/views/ai/data/user/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <doc-alert title="地区 & IP" url="https://doc.iocoder.cn/area-and-ip/" />
+</template>
